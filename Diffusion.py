6d7865aa827408007c741bf8fd6fccd3768d56fd
MartyushevVA <101658294+MartyushevVA@users.noreply.github.com>
2022-12-27 19:46:45 +0300

    Add files via upload

--- Diffusion.py
+++ Diffusion.py
@@ -59,40 +59,16 @@ class CellularAutomaton:
             self.random_stats()
 
     def random_stats(self):
-        for i in range(len(self.cells) // 8):
+        for i in range(len(self.cells) // 64):
             random_id = random.randint(0, len(self.cells) - 1)
             self.cells[random_id].status = True
-        self.update_neighbors()
 
-    def update_neighbors(self):
-        for curr_cell in self.cells.keys():
-            count = 0
-            for i in range(self.cells[curr_cell].x - 1, self.cells[curr_cell].x + 2):
-                for j in range(self.cells[curr_cell].y - 1, self.cells[curr_cell].y + 2):
-                    if (i == self.cells[curr_cell].x) and (j == self.cells[curr_cell].y):
-                        pass
-                    else:
-                        if i < 0:
-                            i = self.WIDTH - 1
-                        elif i > self.WIDTH:
-                            i = 0
-                        if j < 0:
-                            j = self.WIDTH - 1
-                        elif j > self.WIDTH:
-                            j = 0
-                        curr_id = j + self.WIDTH * i
-                        try:
-                            if self.cells[curr_id].status:
-                                count += 1
-                        except:
-                            pass
-            self.cells[curr_cell].neighbors_count = count
 
     def update_cells(self, ITER):
+        temp = 0
+        N = W_S / C_S
         if ITER == 0:
             i = 0
-            temp = 0
-            N = W_S / C_S
             border = int(N / 2)
             while i < (N ** 2 - N - 1):
                 for j in range(border):
@@ -112,6 +88,28 @@ class CellularAutomaton:
                     temp = 0
                     i += 2
                 i += N
+
+        if ITER == 0:
+            i = N + 1
+            border = int(N / 2) - 1
+            while i < (N ** 2 - 2 * N - 2):
+                for j in range(border):
+                    rand = random.randint(0, 2)
+                    if rand == 0:
+                        temp = self.cells[i + N].status
+                        self.cells[i + N].status = self.cells[i].status
+                        self.cells[i].status = self.cells[i + 1].status
+                        self.cells[i + 1].status = self.cells[i + N + 1].status
+                        self.cells[i + N + 1].status = temp
+                    if rand == 1:
+                        temp = self.cells[i + N + 1].status
+                        self.cells[i + N + 1].status = self.cells[i + 1].status
+                        self.cells[i + 1].status = self.cells[i].status
+                        self.cells[i].status = self.cells[i + N].status
+                        self.cells[i + N].status = temp
+                    temp = 0
+                    i += 2
+                i += N + 2
 
 
 def main():
@@ -135,6 +133,7 @@ def main():
                 Automaton.cells[result[1][1] + result[1][0] * Automaton.WIDTH].status = False
             Automaton.update_neighbors()
         ITER = 1 - ITER
+        pygame.time.wait(10)
 
 
 if __name__ == "__main__":
