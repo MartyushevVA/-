edeebdedd203db45c77d918787263c8376dec434
MartyushevVA <101658294+MartyushevVA@users.noreply.github.com>
2023-01-08 14:40:03 +0300

    Add files via upload

--- Wild.py
+++ Wild.py
@@ -74,112 +74,122 @@ class CellularAutomaton:
         self.random_stats()
 
     def random_stats(self):
+        #(0, len(self.cells) - 1)
         for i in range(NUMVICTIMS):
-            random_id = random.randint(0, len(self.cells) - 1)
+            random_id = random.randint(self.WIDTH, self.WIDTH * (self.WIDTH - 1) - 1)
             self.cells[random_id].status = 1
             self.victims.append(random_id)
             self.cells[random_id].time_of_reproduction = TIMEVICTIMS
         for i in range(NUMHUNTERS):
-            random_id = random.randint(0, len(self.cells) - 1)
+            random_id = random.randint(self.WIDTH, self.WIDTH * (self.WIDTH - 1) - 1)
             self.cells[random_id].status = 2
             self.hunters.append(random_id)
             self.cells[random_id].time_of_reproduction = TIMEHUNTERS
             self.cells[random_id].time_of_hungry = HUNGRY
         self.closest_opponent()
 
-
     def closest_opponent(self):
         for vic in self.victims:
             minr = 9999
             for hun in self.hunters:
-                if pow((pow((abs(self.cells[vic].x - self.cells[hun].x) - 1), 2) + pow((abs(self.cells[vic].y - self.cells[hun].y) - 1), 2)), 0.5) < minr:
-                    minr = pow((pow((abs(self.cells[vic].x - self.cells[hun].x) - 1), 2) + pow((abs(self.cells[vic].y - self.cells[hun].y) - 1), 2)), 0.5)
+                if pow((pow((abs(self.cells[vic].x - self.cells[hun].x) - 1), 2) + pow(
+                        (abs(self.cells[vic].y - self.cells[hun].y) - 1), 2)), 0.5) < minr:
+                    minr = pow((pow((abs(self.cells[vic].x - self.cells[hun].x) - 1), 2) + pow(
+                        (abs(self.cells[vic].y - self.cells[hun].y) - 1), 2)), 0.5)
                     self.cells[vic].close_opp = hun
             self.cells[vic].location_of_closest_opponent = minr
         for hun in self.hunters:
             minr = 9999
             for vic in self.victims:
-                if pow((pow((abs(self.cells[vic].x - self.cells[hun].x) - 1), 2) + pow((abs(self.cells[vic].y - self.cells[hun].y) - 1), 2)), 0.5) < minr:
-                    minr = pow((pow((abs(self.cells[vic].x - self.cells[hun].x) - 1), 2) + pow((abs(self.cells[vic].y - self.cells[hun].y) - 1), 2)), 0.5)
+                if pow((pow((abs(self.cells[vic].x - self.cells[hun].x) - 1), 2) + pow(
+                        (abs(self.cells[vic].y - self.cells[hun].y) - 1), 2)), 0.5) < minr:
+                    minr = pow((pow((abs(self.cells[vic].x - self.cells[hun].x) - 1), 2) + pow(
+                        (abs(self.cells[vic].y - self.cells[hun].y) - 1), 2)), 0.5)
                     self.cells[hun].close_opp = vic
             self.cells[hun].location_of_closest_opponent = minr
 
-
     def update_cells(self, time):
         ti = time
+        masnaprxy = [[1, 1, 0, -1, -1, -1, 0, +1],
+                     [0, -self.WIDTH, -self.WIDTH, -self.WIDTH, 0, +self.WIDTH, +self.WIDTH, +self.WIDTH]]
+        masnapr = [1, -self.WIDTH + 1, -self.WIDTH, -self.WIDTH - 1, -1, self.WIDTH - 1, self.WIDTH, self.WIDTH + 1]
         for key, item in self.cells.items():
-            napr = random.randint(0, 8)
-            if self.cells[key].status == 2:
-                if self.cells[key].location_of_closest_opponent <= RADHUNTER:
-                    if self.cells[self.cells[key].close_opp].x > self.cells[key].x:
-                        if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
-                            napr = 7
-                        if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
-                            napr = 1
-                        if self.cells[self.cells[key].close_opp].y == self.cells[key].y:
-                            napr = 0
-                    if self.cells[self.cells[key].close_opp].x == self.cells[key].x:
-                        if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
-                            napr = 6
-                        if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
-                            napr = 2
-                    if self.cells[self.cells[key].close_opp].x < self.cells[key].x:
-                        if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
-                            napr = 5
-                        if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
-                            napr = 3
-                        if self.cells[self.cells[key].close_opp].y == self.cells[key].y:
-                            napr = 4
-            if self.cells[key].status == 1:
-                if self.cells[key].location_of_closest_opponent <= RADVICTIMS:
-                    if self.cells[self.cells[key].close_opp].x > self.cells[key].x:
-                        if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
-                            napr = 3
-                        if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
-                            napr = 5
-                        if self.cells[self.cells[key].close_opp].y == self.cells[key].y:
-                            napr = 4
-                    if self.cells[self.cells[key].close_opp].x == self.cells[key].x:
-                        if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
-                            napr = 2
-                        if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
-                            napr = 6
-                    if self.cells[self.cells[key].close_opp].x < self.cells[key].x:
-                        if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
-                            napr = 1
-                        if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
-                            napr = 7
-                        if self.cells[self.cells[key].close_opp].y == self.cells[key].y:
-                            napr = 0
-            temp = Cell(-1, -1)
-            temp = self.cells[key]
-            if napr == 0:
-                self.cells[key] = self.cells[key + 1]
-                self.cells[key + 1] = temp
-            if napr == 1:
-                self.cells[key] = self.cells[key - self.WIDTH + 1]
-                self.cells[key - self.WIDTH + 1] = temp
-            if napr == 2:
-                self.cells[key] = self.cells[key - self.WIDTH]
-                self.cells[key - self.WIDTH] = temp
-            if napr == 3:
-                self.cells[key] = self.cells[key - self.WIDTH - 1]
-                self.cells[key - self.WIDTH - 1] = temp
-            if napr == 4:
-                self.cells[key] = self.cells[key - 1]
-                self.cells[key - 1] = temp
-            if napr == 5:
-                self.cells[key] = self.cells[key + self.WIDTH - 1]
-                self.cells[key + self.WIDTH - 1] = temp
-            if napr == 6:
-                self.cells[key] = self.cells[key + self.WIDTH]
-                self.cells[key + self.WIDTH] = temp
-            if napr == 7:
-                self.cells[key] = self.cells[key + self.WIDTH + 1]
-                self.cells[key + self.WIDTH + 1] = temp
+            if self.cells[key].status:
+                flag = 0
+                napr = 0
+                if self.cells[key].status == 2:
+                    if self.cells[key].location_of_closest_opponent <= RADHUNTER:
+                        flag = 1
+                        if self.cells[self.cells[key].close_opp].x > self.cells[key].x:
+                            if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
+                                napr = 7
+                            if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
+                                napr = 1
+                            if self.cells[self.cells[key].close_opp].y == self.cells[key].y:
+                                napr = 0
+                        if self.cells[self.cells[key].close_opp].x == self.cells[key].x:
+                            if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
+                                napr = 6
+                            if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
+                                napr = 2
+                        if self.cells[self.cells[key].close_opp].x < self.cells[key].x:
+                            if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
+                                napr = 5
+                            if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
+                                napr = 3
+                            if self.cells[self.cells[key].close_opp].y == self.cells[key].y:
+                                napr = 4
+                if self.cells[key].status == 1:
+                    if self.cells[key].location_of_closest_opponent <= RADVICTIMS:
+                        flag = 1
+                        if self.cells[self.cells[key].close_opp].x > self.cells[key].x:
+                            if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
+                                napr = 3
+                            if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
+                                napr = 5
+                            if self.cells[self.cells[key].close_opp].y == self.cells[key].y:
+                                napr = 4
+                        if self.cells[self.cells[key].close_opp].x == self.cells[key].x:
+                            if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
+                                napr = 2
+                            if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
+                                napr = 6
+                        if self.cells[self.cells[key].close_opp].x < self.cells[key].x:
+                            if self.cells[self.cells[key].close_opp].y > self.cells[key].y:
+                                napr = 1
+                            if self.cells[self.cells[key].close_opp].y < self.cells[key].y:
+                                napr = 7
+                            if self.cells[self.cells[key].close_opp].y == self.cells[key].y:
+                                napr = 0
+                if flag == 0:
+                    if (key + 1) % self.WIDTH == 0:
+                        if key // self.WIDTH == 0:
+                            napr = random.randint(4, 6)
+                        if key // self.WIDTH == self.WIDTH - 1:
+                            napr = random.randint(2, 4)
+                        else:
+                            napr = random.randint(2, 6)
+                    if key % self.WIDTH == 0:
+                        if key // self.WIDTH == 0:
+                            tmr = [0, 6, 7]
+                            napr = tmr[random.randint(0, 2)]
+                        if key // self.WIDTH == self.WIDTH - 1:
+                            napr = random.randint(0, 2)
+                        else:
+                            tmr = [0, 1, 2, 6, 7]
+                            napr = tmr[random.randint(0, 4)]
+                    elif key // self.WIDTH == 0:
+                        tmr = [0, 4, 5, 6, 7]
+                        napr = tmr[random.randint(0, 4)]
+                    elif key // self.WIDTH == self.WIDTH - 1:
+                        napr = random.randint(0, 4)
+                    else:
+                        napr = random.randint(0, 7)
 
-
-
+                print(key, napr, self.cells[key].status)
+                if not self.cells[key + masnapr[napr]].status:
+                    self.cells[key + masnapr[napr]] = self.cells[key]
+                    self.cells[key].status = 0
 
 
 def main():
@@ -196,10 +206,10 @@ def main():
         result = Game.check_events()
         if result[0] == 'Start/Stop':
             PLAY = not PLAY
-        # elif result[0] == 'Changing':
-        #     Automaton.cells[result[1][1] + result[1][0] * Automaton.WIDTH].status = (Automaton.cells[
-        #                                                                                  result[1][1] + result[1][
-        #                                                                                      0] * Automaton.WIDTH].status + 1) % 3
+            # elif result[0] == 'Changing':
+            #     Automaton.cells[result[1][1] + result[1][0] * Automaton.WIDTH].status = (Automaton.cells[
+            #                                                                                  result[1][1] + result[1][
+            #                                                                                      0] * Automaton.WIDTH].status + 1) % 3
             Automaton.closest_opponent()
         pygame.time.wait(0)
 
